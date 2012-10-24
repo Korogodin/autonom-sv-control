2bd262a5c98a8858d1bfcef78cc586b77478f266
korogodiniv@gmail.com <korogodiniv@gmail.com@120caba0-970d-5010-8e3b-fd6bf033798e>
2012-10-24 11:55:00 +0000

    Почти заработал ГЛОНАСС

--- CTime.m
+++ CTime.m
@@ -8,6 +8,10 @@ classdef CTime
         DMY
         t
         Stri
+        JDN % Julian Day Number
+        MJDN
+        ST0 % Sidereal time (for 00:00) (mean)
+        S0
     end
     
     methods
@@ -18,6 +22,22 @@ classdef CTime
             t.Year = Year;
             t.DMY = datevec([num2str(Day) '.' num2str(Month) '.' num2str(Year)], 'dd.mm.yyyy');
             t.Stri = sprintf('%02.0f.%02.0f.%.0f', Day, Month, Year);
+            
+            % Julian
+            a = fix((14 - t.Month)/12);
+            y = t.Year + 4800 - a;
+            m = t.Month + 12*a - 3;
+            t.JDN = t.Day + fix((153*m + 2)/5) + 365*y + fix(y/4) - fix(y/100) + fix(y/400) - 32045;
+            t.MJDN = t.JDN - 2400001;
+            
+            %Sidereal time
+            A1 = 24110.54841;
+            A2 = 8640184.812;
+            A3 = 0.093104;
+            A4 = 0.0000062;
+            T0 = (t.MJDN - 51544.5) / 36525;
+            t.ST0 = A1 + A2 * T0 + A3 * T0 ^ 2 - A4 * T0 ^ 3; % sideral time, s
+            t.S0 = deg2rad(mod(t.ST0 *15 / 3600, 360)); % -> rad
         end
         function res = minus(obj1, obj2)
             res = (datenum(obj1.DMY) - datenum(obj2.DMY))*60*60*24;
--- main.m
+++ main.m
@@ -15,8 +15,21 @@ R_e = 6371e3;  % [m] Mean Earth radiius
         
 Tmod = 15*60*60;
 dTmod = 300;
-t = CTime(0:dTmod:Tmod, 26, 09, 2012);
+t = CTime(0:dTmod:Tmod, 24, 09, 2012);
 Nmod = length(t.t);
+
+% global Omega1 Omega2 Omega3 cOmega1 cOmega2 cOmega3
+% Omega1 = 0; Omega2 = 0; Omega3 = 0;
+GLO_Const = LoadGLOConst([pwd '/data/Topcon_120925.agl'], t);
+figure(1)
+hold on
+for j = 1:length(GLO_Const)
+    plot3(GLO_Const(j).x, GLO_Const(j).y, GLO_Const(j).z, 'm')
+    plot3(GLO_Const(j).x(1), GLO_Const(j).y(1), GLO_Const(j).z(1), '*g', 'MarkerSize', 10)
+%     fprintf('%f\n', GLO_Const(j).Alm.Omega);
+end
+hold off
+
 
 GPS_Const = LoadGPSConst([pwd '/data/Topcon_120925.agp'], t);
 figure(1)
@@ -28,66 +41,119 @@ for j = 1:length(GPS_Const)
 end
 hold off
 
+N_GPS = length(GPS_Const);
+N_GLO = length(GLO_Const);
+
 aa = 0;
-for a = (R_e+0e3):1e3:(R_e + 3.5e6)
-aa = aa+1;
+h_min = 0e3;
+h_max = 5.0e6;
+dh = 100e3;
+Na = length((R_e+h_min):dh:(R_e + h_max));
 
-fprintf('Calculation for LEO satt...');
-% Космос-2480 — российский разведывательный спутник типа Кобальт-М.
-% http://ru.wikipedia.org/wiki/Космос-2480
-% R_e + 259.5e3 = (1 + e)*a;
-% R_e + 196.4e3 = (1 - e)*a; => a = 6598950; e = 0.0048;
-e = 0.0048;
-% a = 6598950; 
-% a = a+750e3;
-Omega = deg2rad(10);
-omega = 0; M0 = 0; 
-i = deg2rad(81.4);
-LEO_SV =  SpaceVehicle_LEO('LEO', a, e, Omega, omega, i, M0, t);
-LEO_SV.calcKeplerOrbit(t);
-fprintf('OK\n');
-figure(1)
-hold on
-plot3(LEO_SV.x, LEO_SV.y, LEO_SV.z, 'r')
-plot3(LEO_SV.x(1), LEO_SV.y(1), LEO_SV.z(1), '*r', 'MarkerSize', 10)
-hold off
+min_S = nan(1, Na);
+max_S = nan(1, Na);
+proc_S = nan(1, Na);
 
+min_S_GLO = nan(1, Na);
+max_S_GLO = nan(1, Na);
+proc_S_GLO = nan(1, Na);
 
-Sucs_Counter = zeros(1, Nmod);
-for k = 1:Nmod
-    N_GPS = length(GPS_Const);
-    N_GLO = 0;
-    
-    LEO_SV.X = [LEO_SV.x(k); LEO_SV.y(k); LEO_SV.z(k)];
-    LEO_SV.AntM = LEO_SV.X/LEO_SV.r(k);
-    for j = 1:N_GPS
-        GPS_Const(j).X = [GPS_Const(j).x(k); GPS_Const(j).y(k); GPS_Const(j).z(k)];
-        GPS_Const(j).AntM = -GPS_Const(j).X /GPS_Const(j).r(k);
-        SVSV = (GPS_Const(j).X - LEO_SV.X); SVSV = SVSV / norm(SVSV);
-        AntAngleLEO = rad2deg(acos(LEO_SV.AntM' * SVSV));
-        AntAngleGPS = rad2deg(acos(GPS_Const(j).AntM' * (-SVSV)));
-        if (AntAngleLEO < 80) && (AntAngleGPS < 20)
-            Sucs_Counter(k) = Sucs_Counter(k) + 1;
+min_S_GPS = nan(1, Na);
+max_S_GPS = nan(1, Na);
+proc_S_GPS = nan(1, Na);
+
+h_S = nan(1, Na);
+
+for a = (R_e+h_min):dh:(R_e + h_max)
+    aa = aa+1;
+
+    fprintf('Calculation for LEO satt, h = %f\n', a-R_e);
+    % Космос-2480 — российский разведывательный спутник типа Кобальт-М.
+    % http://ru.wikipedia.org/wiki/Космос-2480
+    % R_e + 259.5e3 = (1 + e)*a;
+    % R_e + 196.4e3 = (1 - e)*a; => a = 6598950; e = 0.0048;
+    e = 0.0048;
+    % a = 6598950; 
+    % a = a+750e3;
+    Omega = deg2rad(10);
+    omega = 0; M0 = 0; 
+    i = deg2rad(81.4);
+    LEO_SV =  SpaceVehicle_LEO('LEO', a, e, Omega, omega, i, M0, t);
+    LEO_SV.calcKeplerOrbit(t);
+    fprintf('OK\n');
+    figure(1)
+    hold on
+    plot3(LEO_SV.x, LEO_SV.y, LEO_SV.z, 'r')
+    plot3(LEO_SV.x(1), LEO_SV.y(1), LEO_SV.z(1), '*r', 'MarkerSize', 10)
+    hold off
+
+    Sucs_Counter_GLO = zeros(1, Nmod);
+    Sucs_Counter_GPS = zeros(1, Nmod);
+    for k = 1:Nmod
+
+        LEO_SV.X = [LEO_SV.x(k); LEO_SV.y(k); LEO_SV.z(k)];
+        LEO_SV.AntM = LEO_SV.X/LEO_SV.r(k);
+        
+        for j = 1:N_GPS
+            GPS_Const(j).X = [GPS_Const(j).x(k); GPS_Const(j).y(k); GPS_Const(j).z(k)];
+            GPS_Const(j).AntM = -GPS_Const(j).X /GPS_Const(j).r(k);
+            SVSV = (GPS_Const(j).X - LEO_SV.X); SVSV = SVSV / norm(SVSV);
+            AntAngleLEO = rad2deg(acos(LEO_SV.AntM' * SVSV));
+            AntAngleGPS = rad2deg(acos(GPS_Const(j).AntM' * (-SVSV)));
+            if (abs(AntAngleLEO) <= 85) && (abs(AntAngleGPS) <= 25)
+                Sucs_Counter_GPS(k) = Sucs_Counter_GPS(k) + 1;
+            end
+        end
+        
+        for j = 1:N_GLO
+            GLO_Const(j).X = [GLO_Const(j).x(k); GLO_Const(j).y(k); GLO_Const(j).z(k)];
+            GLO_Const(j).AntM = -GLO_Const(j).X /GLO_Const(j).r(k);
+            SVSV = (GLO_Const(j).X - LEO_SV.X); SVSV = SVSV / norm(SVSV);
+            AntAngleLEO = rad2deg(acos(LEO_SV.AntM' * SVSV));
+            AntAngleGLO= rad2deg(acos(GLO_Const(j).AntM' * (-SVSV)));
+            if (abs(AntAngleLEO) <= 85) && (abs(AntAngleGLO) <= 25)
+                Sucs_Counter_GLO(k) = Sucs_Counter_GLO(k) + 1;
+            end        
         end
     end
-    
-end
-figure(2)
-plot(t.t, Sucs_Counter)
 
-min_S(aa) = min(Sucs_Counter);
-max_S(aa) = max(Sucs_Counter);
-proc_S(aa) = sum(Sucs_Counter>=4) / Nmod * 100;
-h_S(aa) = a - R_e;
-clear LEO_SV;
+    Sucs_Counter = Sucs_Counter_GLO + Sucs_Counter_GPS;
+
+    figure(2)
+    plot(t.t, Sucs_Counter, t.t, Sucs_Counter_GPS, t.t, Sucs_Counter_GLO)
+    xlabel('t, sec');
+    legend('Sum', 'GPS', 'GLO');
+    
+    min_S(aa) = min(Sucs_Counter);
+    max_S(aa) = max(Sucs_Counter);
+    proc_S(aa) = sum(Sucs_Counter>=4) / Nmod * 100;
+
+    min_S_GLO(aa) = min(Sucs_Counter_GLO);
+    max_S_GLO(aa) = max(Sucs_Counter_GLO);
+    proc_S_GLO(aa) = sum(Sucs_Counter_GLO>=4) / Nmod * 100;
+
+    min_S_GPS(aa) = min(Sucs_Counter_GPS);
+    max_S_GPS(aa) = max(Sucs_Counter_GPS);
+    proc_S_GPS(aa) = sum(Sucs_Counter_GPS>=4) / Nmod * 100;
+
+    h_S(aa) = a - R_e;
+    clear LEO_SV;
 end
 
 figure(3)
-plot(h_S/1000, min_S, h_S/1000, max_S)
+plot(h_S/1000, min_S, h_S/1000, min_S_GPS, h_S/1000, min_S_GLO)
 xlabel('H, km')
-ylabel('Min and Max number of visible GPS SV');
+ylabel('Min number of visible SVs');
+legend('Sum', 'GPS', 'GLO');
 
 figure(4)
-plot(h_S/1000, proc_S)
+plot(h_S/1000, max_S, h_S/1000, max_S_GPS, h_S/1000, max_S_GLO)
+xlabel('H, km')
+ylabel('Max number of visible SVs');
+legend('Sum', 'GPS', 'GLO');
+
+figure(5)
+plot(h_S/1000, proc_S, h_S/1000, proc_S_GPS, h_S/1000, proc_S_GLO)
 xlabel('H, km')
 ylabel('The percentage of time during which it is possible positioning (N>=4)');
+legend('Sum', 'GPS', 'GLO');
